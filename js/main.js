d19876fce325f1091783eb28926a83a1adba1617
martinagirgis <martinasaid2018@gmail.com>
2019-09-17 13:43:34 +0200

    add navbar about section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
+$(window).resize(function() {
+            resize();
+        });
+        $(function() {
+            $('.thome').attr('src', 'http://cdn.com/bg.png').css({
+                // 'position' : 'fixed',
+                'top' : 0,
+                'left' : 0
+            });
+            resize();             
+        });
 
+        function resize() {
+            var $img = $('.thome');
+            
+                $img.css({
+                    'width' : '100%',
+                    'height' : $(window).height() + "px" 
+                });
+        }
